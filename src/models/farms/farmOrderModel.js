ca57c576c4ddf5a620b4a6fb4fdb9281d6e384ba
Abass Olaiya <ayotundeolaiya@gmail.com>
2024-11-17 18:14:04 +0100

    pull7

--- src/models/farms/farmOrderModel.js
+++ src/models/farms/farmOrderModel.js
@@ -1,29 +1,17 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
-=======
-import mongoose from 'mongoose';
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
 
 const farmOrderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
       ref: "User", // Reference to the user who placed the order
-=======
-      ref: 'User', // Reference to the user who placed the order
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
       required: true,
     },
     items: [
       {
         product: {
           type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
           ref: "FarmProduct",
-=======
-          ref: 'FarmProduct',
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
           required: true,
         },
         quantity: {
@@ -38,31 +26,18 @@ const farmOrderSchema = mongoose.Schema(
     },
     status: {
       type: String,
-<<<<<<< HEAD
       enum: ["Paid", "Delivered", "In-Transit", "Canceled", "Packed"],
       default: "Paid", // Default status when the order is created
-=======
-      enum: ['Paid', 'Delivered', 'In-Transit', 'Canceled', 'Packed'],
-      default: 'Paid', // Default status when the order is created
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
       required: true,
     },
     deliveryOption: {
       type: String,
-<<<<<<< HEAD
       enum: ["Delivery", "pickup", "Pickup"],
-=======
-      enum: ['Delivery', 'pickup', "Pickup"],
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
       required: true,
     },
     logisticsVehicle: {
       type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
       ref: "LogisticsVehicle",
-=======
-      ref: 'LogisticsVehicle',
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
     },
     deliveryAddress: {
       type: String,
@@ -73,10 +48,6 @@ const farmOrderSchema = mongoose.Schema(
   }
 );
 
-<<<<<<< HEAD
 const FarmOrder = mongoose.model("FarmOrder", farmOrderSchema);
-=======
-const FarmOrder = mongoose.model('FarmOrder', farmOrderSchema);
->>>>>>> de3b8fe9d917dc059a46d1ceaad1bff46b432880
 
 export default FarmOrder;
